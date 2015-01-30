e26349b06a43b27c63b81e298878ad742ffa7655
Tom <uninet@live.no>
2015-01-30 11:08:10 +0100

    added Lokale med metoder

--- polymorfi_opg5/src/lokaler/Lokale.java
+++ polymorfi_opg5/src/lokaler/Lokale.java
@@ -1,5 +1,23 @@
 package lokaler;
 
-public class Lokale {
+public abstract class Lokale {
 
+	private int areal = 0;
+	private int kapacitet = 0;
+	
+	public Lokale(int areal, int kapacitet) {
+		this.areal = areal;
+		this.kapacitet = kapacitet;
+	}
+	
+	public Lokale(Lokale other) {
+		areal = other.areal;
+		kapacitet = other.kapacitet;
+	}
+
+	@Override
+	public String toString() {
+		return "Lokale [areal=" + areal + ", kapacitet=" + kapacitet + "]";
+	}
+	
 }
